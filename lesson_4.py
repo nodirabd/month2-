0c9468ca511fdf89bc8602ac410662292280b7b6
Abdurakhmanova Nodirabegim <abdurakhmanovanodirabegim@gmail.com>
2026-03-10 22:59:14 +0600

    lesson 4 10.03

---
+++ lesson_4.py
@@ -0,0 +1,12 @@
+"""from abc import ABC, abstractmethod
+class Animal:
+    @abstractmethod
+    def make_sound(self):
+        pass
+
+#my_animal = Animal()
+class Dog(Animal):
+    pass
+"""
+#SOLID- набор твердых правил
+#Patterns =шаблоны программирования
