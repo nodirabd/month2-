036314ecf77f916b17442ba4a6c882da708c4069
Abdurakhmanova Nodirabegim <abdurakhmanovanodirabegim@gmail.com>
2026-03-27 00:13:34 +0600

    hw8 and hw7 update

--- homeworks/homework_8.py
+++ homeworks/homework_8.py
@@ -1,9 +1,12 @@
 import sqlite3
 
 
-def create_table(conn):
+def setup_db(conn):
+    # для удаления таблицы каждый раз при запуске
+    conn.execute('DROP TABLE IF EXISTS books')
+
     conn.execute('''
-        CREATE TABLE IF NOT EXISTS books (
+        CREATE TABLE books (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT,
             author TEXT,
@@ -13,36 +16,40 @@ def create_table(conn):
             number_of_copies INTEGER
         )
     ''')
-    conn.commit()
 
-def insert_books(conn, books_list):
-    conn.executemany('''
-        INSERT INTO books (name, author, publication_year, 
-        genre, number_of_pages, number_of_copies)
-        VALUES (?, ?, ?, ?, ?, ?)
-    ''', books_list)
-    conn.commit()
+    books = [
 
-
-if __name__ == "__main__":
-    connection = sqlite3.connect('homework_7.db')
-
-    books_to_add = [
+        ('Белый пароход', 'Чынгыз Айтматов', 1970, 'Повесть', 192, 5),
         ('Кыямат', 'Чынгыз Айтматов', 1986, 'Роман', 450, 10),
-        ('Sapiens', 'Юваль Ной Харари', 2011, 'Научпоп', 520, 7),
         ('Дюна', 'Фрэнк Герберт', 1965, 'Фантастика', 700, 4),
         ('Цветы для Элджернона', 'Дэниел Киз', 1966, 'Драма', 320, 6),
         ('Мастер и Маргарита', 'Михаил Булгаков', 1967, 'Классика', 480, 5),
-        ('1984', 'Джордж Оруэлл', 1949, 'Антиутопия', 328, 12),
         ('Норвежский лес', 'Харуки Мураками', 1987, 'Роман', 380, 3),
         ('Краткие ответы на большие вопросы', 'Стивен Хокинг', 2018, 'Наука', 256, 8),
         ('Портрет Дориана Грея', 'Оскар Уайльд', 1890, 'Классика', 320, 4),
         ('Алхимик', 'Пауло Коэльо', 1988, 'Притча', 208, 9)
     ]
+    conn.executemany(
+        'INSERT INTO books ('
+        'name, author, publication_year, '
+        'genre, number_of_pages, number_of_copies)'
+        ' VALUES (?, ?, ?, ?, ?, ?)',
+        books)
+    conn.commit()
 
-    create_table(connection)
-    insert_books(connection, books_to_add)
 
-    connection.close()
+def get_books_by_author(conn, author):
+    cursor = conn.execute('SELECT name FROM books WHERE author = ? '
+                          'ORDER BY name ASC', (author,))
+    return cursor.fetchall()
 
-    print("База данных успешно обновлена. Добавлено 10 книг.")
+
+conn = sqlite3.connect("homework_8.db")
+setup_db(conn)  # Теперь таблица будет обновляться при каждом запуске
+
+author = "Чынгыз Айтматов"
+print(f"Список книг {author}:")
+for row in get_books_by_author(conn, author):
+    print(f"- {row[0]}")
+
+conn.close()
